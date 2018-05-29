c40c1b12dafadb8919b5d9dae44004c90d4196f4
Valentin Pelloin <valentinpelloin@gmail.com>
2018-05-29 20:34:28 +0200

    counting up

--- muscles/Telescope/src/main.cpp
+++ muscles/Telescope/src/main.cpp
@@ -10,17 +10,16 @@ ScreenLed * ledEmergency;
 ScreenLed * ledRunning;
 ScreenLed * ledArmed;
 
+unsigned long begin;
+int i = 0;
+
 void setup() {
     s = Screen::getInstance();
 	s->setRotation(3);
 	s->fillRoundRect(0, 0, s->width(), s->height(), 20, COLOR_WHITE);
 
-	s->setTextColor(COLOR_BLACK);
+	s->setTextColor(COLOR_BLACK, COLOR_LIGHTGREY);
 	s->setTextSize(2);
-	s->setCursor(10, 2);
-
-	const char * somebody = "Valentin";
-	s->print("Hello %s!", somebody);
 
 	ledEmergency = new ScreenLed(s, 100, 50, 10, COLOR_RED);
 	ledEmergency->setUnactive();
@@ -30,10 +29,14 @@ void setup() {
 
 	ledArmed     = new ScreenLed(s, 200, 50, 10, COLOR_BLUE);
 	ledArmed->setBlinking(250);
+	begin = millis();
 }
 
 void loop() {
 	ledEmergency->refresh();
 	ledRunning->refresh();
 	ledArmed->refresh();
+	s->setCursor(0, 0);
+	s->print("Count: %i (%is since start)", i, (millis() - begin) / 1000);
+	i++;
 }
